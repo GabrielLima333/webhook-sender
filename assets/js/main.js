19c512bc92d8260a8c8296e5d0974af5f5fbad18
Júpiter <mokuseidev@gmail.com>
2021-10-30 10:59:43 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
+$(function() {
+	$('#send').click(function() {
+		var url = $('#url').val();
+		var avatar_url = $('#avatar_url').val();
+		var username = $('#username').val();
+		var content = $('#content').val();
 
+		if (url == null || url == "", content == null || content == "") {
+			alert("Please fill the all fields!");
+			return false;
+		}
+
+		$.post(url, {
+			"avatar_url": avatar_url,
+			"username": username,
+			"content": content
+		});
+	});
+});
